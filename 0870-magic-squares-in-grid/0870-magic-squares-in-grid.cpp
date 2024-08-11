57680493c0f56883851521f0fe8f94649244187e
Lokranjan P <2021is_lokranjanp_a@nie.ac.in>
2024-08-11 07:45:58 +0530

    Time: 0 ms (100%), Space: 10.9 MB (59.21%) - LeetHub

---
+++ 0870-magic-squares-in-grid/0870-magic-squares-in-grid.cpp
@@ -0,0 +1,49 @@
+class Solution {
+    bool valid(int i, int j, vector<vector<int>>& grid) {
+        vector<int> element(10, 0);
+        for (int x = 0; x < 3; ++x) {
+            for (int y = 0; y < 3; ++y) {
+                int num = grid[i + x][j + y];
+                if (num < 1 || num > 9 || element[num] > 0) 
+                    return false;
+                element[num]++;
+            }
+        }
+
+        int sum = grid[i][j] + grid[i][j+1] + grid[i][j+2];
+        
+        for(int x = 0; x < 3; ++x) {
+            if(sum != (grid[i + x][j] + grid[i + x][j + 1] + grid[i + x][j + 2]))
+                return false;
+        }
+        
+        for(int y = 0; y < 3; ++y) {
+            if(sum != (grid[i][j + y] + grid[i + 1][j + y] + grid[i + 2][j + y])) 
+                return false;
+        }
+        
+        if(sum != (grid[i][j] + grid[i+1][j+1] + grid[i+2][j+2])) 
+            return false;
+        if(sum != (grid[i+2][j] + grid[i+1][j+1] + grid[i][j+2])) 
+            return false;
+
+        return true;
+    }
+    
+public:
+    int numMagicSquaresInside(vector<vector<int>>& grid) {
+        int res = 0;
+        int row = grid.size();
+        int col = grid[0].size();
+        
+        if(row < 3 || col < 3) return 0;
+
+        for(int i = 0; i <= row - 3; ++i) {
+            for(int j = 0; j <= col - 3; ++j) {
+                if(valid(i, j, grid))  
+                    res++;
+            }
+        }
+        return res;
+    }
+};
