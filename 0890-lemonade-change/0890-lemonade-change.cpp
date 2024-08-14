73efc4bffec23a2b8a21fee11d6491039abcbe2e
Lokranjan P <2021is_lokranjanp_a@nie.ac.in>
2024-08-14 15:08:04 +0530

    Time: 68 ms (74.13%), Space: 86.1 MB (31.99%) - LeetHub

---
+++ 0890-lemonade-change/0890-lemonade-change.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        int fives = 0;
+        int tens = 0;
+
+        for(int i = 0; i<bills.size(); ++i)
+        {
+            if(bills[i] == 5)
+                fives++;
+            
+            if(bills[i] == 10)
+            {
+                if(fives>0)
+                {
+                    fives--;
+                    tens++;
+                }
+                else
+                    return false;
+            }
+            else if(bills[i] == 20)
+            {
+                if(fives>0 && tens>0)
+                {
+                    fives--;
+                    tens--;
+                }
+                else if(fives>=3)
+                    fives-=3;
+                else
+                    return false;
+            }
+        }
+        return true;
+    }
+};
