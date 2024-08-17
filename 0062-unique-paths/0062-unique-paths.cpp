5397187cb11fa2d48c1b1206a4a8f63b9b109460
Lokranjan P <2021is_lokranjanp_a@nie.ac.in>
2024-08-17 22:45:10 +0530

    Time: 0 ms (100%), Space: 8 MB (13.74%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -0,0 +1,19 @@
+class Solution {
+private:
+    int helper(int i, int j, vector<vector<int>>&dp)
+    {
+        if(i<=0 || j<=0)
+            return 1;
+
+        if(dp[i][j]!=-1)
+            return dp[i][j];
+
+        return dp[i][j] = helper(i-1, j, dp)+helper(i, j-1, dp);
+    }
+
+public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int>>dp(m, vector<int>(n, -1));
+        return helper(m-1, n-1, dp);
+    }
+};
