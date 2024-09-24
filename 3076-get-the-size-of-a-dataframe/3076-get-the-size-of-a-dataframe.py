0e49cb4ea3e75facbb5c34429adb9d02703f7bae
Lokranjan P <2021is_lokranjanp_a@nie.ac.in>
2024-09-24 20:40:06 +0530

    Time: 378 ms (48.06%), Space: 68.9 MB (69.73%) - LeetHub

--- 3076-get-the-size-of-a-dataframe/3076-get-the-size-of-a-dataframe.py
+++ 3076-get-the-size-of-a-dataframe/3076-get-the-size-of-a-dataframe.py
@@ -1,8 +1,4 @@
 import pandas as pd
 
-def getDataframeSize(players: pd.DataFrame) -> List[int]:
-    dataframe_shape = []
-    dataframe_shape.append(players.shape[0])
-    dataframe_shape.append(players.shape[1])
-
-    return dataframe_shape
+def getDataframeSize(players: pd.DataFrame) -> List[int]:    
+    return [players.shape[0], players.shape[1]] 
